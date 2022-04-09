a99e6a721207377a9ec15fda0a6c470a713e7007
Derock <derock@derock.dev>
2022-04-09 11:13:52 -0400

    finish

--- src/pages/home/Images.tsx
+++ src/pages/home/Images.tsx
@@ -4,13 +4,10 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 const images = [
-    [require("../../assets/img/feat1.png"), '/profile » Shows lots of data related to a summoner.'],
-    [require("../../assets/img/feat1.png"), "PLACEHOLDER"],
-    [require("../../assets/img/feat1.png"), "PLACEHOLDER"],
-    [require("../../assets/img/feat1.png"), "PLACEHOLDER"],
-    [require("../../assets/img/feat1.png"), "PLACEHOLDER"],
-    [require("../../assets/img/feat1.png"), "PLACEHOLDER"],
-    [require("../../assets/img/pagebg.jpg"), 'test']
+    [require("../../assets/img/features/profilecmd.png"), '/profile & /masteries » Quickly view lots of data about a summoner.'],
+    [require("../../assets/img/features/champions.png"), "/champion & /counters & /synergies » Quickly view lots of data about a champion."],
+    [require("../../assets/img/features/match.png"), "/matches » Check a summoners previous matches and match data."],
+    [require("../../assets/img/features/patchnotes.png"), "/patchnotes » Automatically receive the latest patch notes on any channel!"],
 ]
 
 export default function Images() {
@@ -41,7 +38,7 @@ export default function Images() {
             {
                 images.map(([src, alt], index) => (
                     <div className="h-[60vh] w-auto block" key={index}>
-                        <img {...{src, alt}} className="h-auto max-h-[80%] md:max-h-[90%] w-auto object-contain" loading="lazy" />
+                        <img {...{src, alt}} className="h-auto max-h-[80%] md:max-h-[90%] w-auto object-contain image-shadow" loading="lazy" />
                         <p className="text-white text-shadow-lg">{alt}</p>
                     </div>
                 ))
