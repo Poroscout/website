ff5157a890da4fa1351e3c71c26b41ac2f23dfe4
Derock <derock@derock.dev>
2022-04-25 18:52:03 -0400

    update: tos

--- src/pages/tos/Terms.tsx
+++ src/pages/tos/Terms.tsx
@@ -8,14 +8,14 @@ function TermsOfService() {
 
       <div className="space-y-1">
         <h1 className="text-2xl text-center">Terms of Use and Privacy Policy</h1>
-        <p className="text-center italic">Updated: 4/25/2022 at 9:45 EST</p>
+        <p className="text-center italic">Updated: 4/25/2022 at 6:50 EST</p>
       </div>
       
       <section>
         <h2 className="mb-2 text-lg font-semibold">Usage Agreement:</h2>
         <p>
-          By inviting this bot and/or use the commands of the bot, you agree that you have read and understand the Terms of Service and Privacy Policy of the bot. 
-          <br /> You also agree that you will use this bot in a manner that is in compliance with the Discord <a href="https://discord.com/terms" className="text-blue-800">Terms of Service</a> and <a href="https://discord.com/guidelines" className="text-blue-800">Community Guidelines</a>.
+          By inviting this bot and/or using the commands of the bot, you agree that you have read and understood the Terms of Service and Privacy Policy of the bot. 
+          <br /> You also agree that you will use this bot in a manner that complies with the Discord's <a href="https://discord.com/terms" className="text-blue-800">Terms of Service</a> and <a href="https://discord.com/guidelines" className="text-blue-800">Community Guidelines</a>.
           <br /> We take no responsibility for any actions or omissions of the bot and are not liable for individuals who use the bot to break any of Discord's <a href="https://discord.com/terms" className="text-blue-800">Terms of Service</a> or <a href="https://discord.com/guidelines" className="text-blue-800">Community Guidelines</a>.
         </p>
         <br />
@@ -49,14 +49,24 @@ function TermsOfService() {
         </ul>
         <br />
         <p>
-          PoroScout will never share your any personal identifying information publicly without your concent unless required by law.
+          Your data is stored securly in our database. Stored data is also <strong>encrypted</strong> for added protection.
+          <br /> In the unlikely event of a data breach, we will send a notification in our Discord server that includes the extent of the data breach.
+        </p>
+        <br />
+        <p>
+          As per Discord's guidelines, any personal identifying information you provide will only be stored for 30 days.
+          <br /> It will automatically be deleted after 30 days (processed at midnight EST).
+        </p>
+        <br />
+        <p>
+          PoroScout will never share any personal identifying information publicly without your concent unless required by law.
           <br/> To unlink your League of Legends username, region, and ID from your Discord account, you can run <strong>/unlink</strong>.
           <br/> Have any questions? Feel free to email us at <a href="mailto:derock@derock.dev" className="font-bold">derock@derock.dev</a>.
         </p>
         <br />
         <p>
-          Our bot/website may contain links to third party websites that are not operated by us.
-          <br/> We have no control over, and assume no responsibility for, the content, privacy policies, or practices of any third party sites or services. 
+          Our bot/website may contain links to third-party websites that are not operated by us.
+          <br/> We have no control over and assume no responsibility for, the content, privacy policies, or practices of any third party sites or services. 
         </p>
       </section>
 
@@ -73,10 +83,10 @@ function TermsOfService() {
         <p>
           PoroScout is also not affiliated with Discord.
           Discord is a registered trademark of Discord, Inc.
-          Any direct connection to Discord and/or it's trademark 
+          Any direct connection to Discord and/or its trademark 
           is purely coincidental. 
           We do not claim to have any copyright
-          ownership of Discord and/or it's assets.
+          ownership of Discord and/or its assets.
         </p>
       </section>
       
