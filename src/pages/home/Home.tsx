be5a8b038b52ce9879bde5fb024584653b4fa374
Derock <derock@derock.dev>
2022-04-25 10:34:51 -0400

    fix: scrollbar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
 
-import "../../assets/css/home.css";
+// import "../../assets/css/home.scoped.css";
 
 function Home() {
+  useEffect(() => {
+    console.log('asdf')
+  }, []);
+
   return (
-    <div className="hero w-screen h-screen">
-      <div className="hero-grid w-screen h-screen">
-        <div className="hero-overlay w-screen h-screen">
+    <div className="hero w-screen h-screen overflow-hidden">
+      <div className="hero-grid w-screen h-screen overflow-hidden">
+        <div className="hero-overlay w-screen h-screen overflow-hidden">
           <Header />
           <Hero />
           <Footer />
