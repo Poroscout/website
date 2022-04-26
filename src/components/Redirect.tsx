88c33a9f3ca46a8d21d044bb28e4427f2c82c862
Derock <derock@derock.dev>
2022-04-26 16:54:25 -0400

    fix

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 export default function Redirect(props: { to: string }) {
     useEffect(() => {
         window.location.href = props.to
-    }, []);
+    }, [props.to]);
 
     return (
         <p>Redirecting... Click <a className="text-blue-600" href={props.to}>here</a> if you are not redirected automatically.</p>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+// import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
