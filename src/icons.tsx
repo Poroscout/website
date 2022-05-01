218a4b2dae215aab6e1baa29b9294142be774ce6
Derock <derock@derock.dev>
2022-04-30 21:02:06 -0400

    add 404

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,10 +1,10 @@
 import fontawesome from '@fortawesome/fontawesome'
-import { faCaretLeft, faCaretRight, faUserPlus } from '@fortawesome/fontawesome-free-solid'
+import { faCaretLeft, faCaretRight, faUserPlus, faHome } from '@fortawesome/fontawesome-free-solid'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 fontawesome.library.add(
     // @ts-ignore
-    faCaretLeft, faCaretRight, faUserPlus, fab
+    faCaretLeft, faCaretRight, faUserPlus, fab, faHome
 )
 
 export {}
--- src/pages/home/Header.tsx
+++ src/pages/home/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="top-2 w-full text-center text-white font-semibold text-xl hidden sm:block absolute"> 
       <p className="w-fit">
-        <Link to="#">
+        <Link to="/">
           <img src={require('../../assets/img/logo.png')} alt="logo" className="h-12 w-12 inline mx-2" />
           <span className="align-middle">PoroScout</span>
         </Link>
