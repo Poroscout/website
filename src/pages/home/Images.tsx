1b138b9db53f8ef772ab7a3a844b8f4fb7df96cb
Derock <derock@derock.dev>
2022-06-23 09:32:01 -0400

    feat: add graphs to images

--- src/pages/home/Images.tsx
+++ src/pages/home/Images.tsx
@@ -7,33 +7,12 @@ const images = [
     [require("../../assets/img/features/profilecmd.png"), '/profile & /masteries » Quickly view lots of data about a summoner.'],
     [require("../../assets/img/features/champions.png"), "/champion & /counters & /synergies » Quickly view lots of data about a champion."],
     [require("../../assets/img/features/match.png"), "/matches » Check a summoners previous matches and match data."],
+    [require("../../assets/img/features/graphs.png"), "/graph & /mmr » Check and graph a player's MMR and LP!"],
     [require("../../assets/img/features/patchnotes.png"), "/patchnotes » Automatically receive the latest patch notes on any channel!"],
 ]
 
 export default function Images() {
-    // const [image, setImage] = useState(0);
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setImage(image => (image + 1) % images.length);
-    //     }, 5000);
-
-    //     return () => clearInterval(interval);
-    // }, []);
-
     return (
-        // <div className="carousel grid gap-4 grid-cols-1 sm:grid-cols-3 grid-rows-1 px-3">
-        //     <FontAwesomeIcon icon="fa-solid fa-caret-left" className="text-white text-right m-auto mr-4 h-20 opacity-40 hidden sm:block" />
-        //     <div>
-        //         <img src={images[image][0]} alt={images[image][1]} className="w-full" />
-        //         <p className="w-fit sm:w-full text-gray-200">
-        //             <FontAwesomeIcon icon="fa-solid fa-caret-right" className="text-white text-right m-auto mr-4 h-10 opacity-40 block sm:hidden" />
-        //             {images[image][1]}
-        //             <FontAwesomeIcon icon="fa-solid fa-caret-left" className="text-white text-left m-auto ml-4 h-10 opacity-40 block sm:hidden" />
-        //         </p>
-        //     </div>
-        //     <FontAwesomeIcon icon="fa-solid fa-caret-right" className="text-white text-left m-auto ml-4 h-20 opacity-40 hidden sm:block" />
-        // </div>
         <Carousel className="max-w-32 mt-5 w-80 sm:w-[60vw] m-auto" autoPlay infiniteLoop interval={5000} showThumbs={false} emulateTouch transitionTime={800} showArrows showIndicators={false} swipeable stopOnHover={false}>
             {
                 images.map(([src, alt], index) => (
