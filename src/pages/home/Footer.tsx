9f9d2a91c1f8831451be09ec7abda66fef8a1ea9
Derock <derock@derock.dev>
2022-07-02 20:26:04 -0400

    feat: prepare for public release

--- src/pages/home/Footer.tsx
+++ src/pages/home/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="absolute bottom-2 w-full text-center text-gray-300">
       <p>
         Developed with ❤️ by <a className="text-blue-300" href="https://derock.dev">Derock</a> •{" "}
-        <a className="text-blue-300" href="https://github.com/ItzDerock">Github</a> •{" "}
+        <a className="text-blue-300" href="https://github.com/Poroscout">Github</a> •{" "}
         <a className="text-blue-300" href="https://ko-fi.com/derock">Donate</a> •{" "}
         <a className="text-blue-300" href="mailto:derock@derock.dev">Email</a> •{" "}
         {/* <a className="text-blue-300" href="/terms">Terms</a> */}
--- src/pages/tos/Footer.tsx
+++ src/pages/tos/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full text-center text-gray-600">
       <p>
         Developed with ❤️ by <a className="text-blue-600" href="https://derock.dev">Derock</a> •{" "}
-        <a className="text-blue-600" href="https://github.com/ItzDerock">Github</a> •{" "}
+        <a className="text-blue-600" href="https://github.com/Poroscout">Github</a> •{" "}
         <a className="text-blue-600" href="https://ko-fi.com/derock">Donate</a> •{" "}
         <a className="text-blue-600" href="mailto:derock@derock.dev">Email</a>
       </p>
