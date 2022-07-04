e010fdc98a2bf328486b68b16ba7fe6cc3dca1bb
Derock <derock@derock.dev>
2022-07-04 08:40:09 -0400

    chore(deps): bump fontawesome; fix: dep conflict

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,11 +1,13 @@
-import fontawesome from '@fortawesome/fontawesome'
-import { faCaretLeft, faCaretRight, faUserPlus, faHome } from '@fortawesome/fontawesome-free-solid'
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faCaretLeft, faCaretRight, faUserPlus, faHome } from '@fortawesome/fontawesome-free-solid';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
-fontawesome.library.add(
+library.add(
     // @ts-ignore
     faCaretLeft, faCaretRight, faUserPlus, fab, faHome, faArrowUpRightFromSquare
 )
+
+dom.watch();
 
 export {}
