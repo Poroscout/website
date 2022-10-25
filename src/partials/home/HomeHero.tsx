c8ffc2ab128ba7688b047480ca2ea1e4dc1ae4ed
Derock <derock@derock.dev>
2022-10-25 15:01:36 -0400

    fix: mobile responsiveness

--- src/partials/home/HomeHero.tsx
+++ src/partials/home/HomeHero.tsx
@@ -5,7 +5,7 @@ export default function HomeHero() {
   return (
     <header>
       <GradientImage image={lolImage}>
-        <div className="text-center drop-shadow-md md:scale-105">
+        <div className="text-center drop-shadow-md md:scale-105 px-4">
           <h2 className="text-slate-200 font-semibold tracking-wide uppercase text-md">
             Piltover&apos;s Finest
           </h2>
@@ -16,7 +16,7 @@ export default function HomeHero() {
             PoroScout is a Discord bot that provides League of Legends summoner and champion
             information and statistics.
           </p>
-          <div className="mt-6">
+          <div className="mt-6 space-y-2">
             <a
               href="/invite"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
