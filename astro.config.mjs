e1808dde6ec0f9ad1b7cc6183a769d99cd019e2f
Derock <derock@derock.dev>
2026-06-06 20:56:52 -0400

    feat: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,42 +38,40 @@ export default defineConfig({
     }),
   },
 
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Montserrat",
-        cssVariable: "--font-montserrat",
-        fallbacks: ["sans-serif"],
-        weights: ["500", "600", "900"],
-        styles: ["normal"],
-        subsets: ["latin"],
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Libre Baskerville",
-        cssVariable: "--font-libre-baskerville",
-        fallbacks: ["sans-serif"],
-        weights: ["400"],
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Karla",
-        cssVariable: "--font-karla",
-        fallbacks: ["sans-serif"],
-        weights: ["400", "700"],
-        styles: ["normal", "italic"],
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Crimson Text",
-        cssVariable: "--font-crimson-text",
-        fallbacks: ["serif"],
-        weights: ["400", "700"],
-        styles: ["normal", "italic"],
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Montserrat",
+      cssVariable: "--font-montserrat",
+      fallbacks: ["sans-serif"],
+      weights: ["500", "600", "900"],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Libre Baskerville",
+      cssVariable: "--font-libre-baskerville",
+      fallbacks: ["sans-serif"],
+      weights: ["400"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Karla",
+      cssVariable: "--font-karla",
+      fallbacks: ["sans-serif"],
+      weights: ["400", "700"],
+      styles: ["normal", "italic"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Crimson Text",
+      cssVariable: "--font-crimson-text",
+      fallbacks: ["serif"],
+      weights: ["400", "700"],
+      styles: ["normal", "italic"],
+    },
+  ],
 
   integrations: [sitemap(), mdx(), icon()],
 });
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A highly reproducible Node.js development environment flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+        };
+      }
+    );
+}
